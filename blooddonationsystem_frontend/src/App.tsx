a6e35c4d2c479ed56a32bbd4c8f8f118bba0c3d4
HienNguyen111 <hiennttse183023@fpt.edu.vn>
2025-06-02 23:03:50 +0700

    2/6 cuu moi

--- blooddonationsystem_frontend/src/App.tsx
+++ blooddonationsystem_frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
+
+
 import Reset1Page from "./pages/ResetPassemail";
 import Reset2Page from "./pages/ResetPassOTP";
 import Reset3Page from "./pages/ResetPass";
@@ -13,6 +15,8 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/login" element={<LoginPage />} />
+
+
 				<Route path="/forgot" element={<Reset1Page />} />
 				<Route path="/forgot2" element={<Reset2Page />} />
 				<Route path="/forgot3" element={<Reset3Page />} />
