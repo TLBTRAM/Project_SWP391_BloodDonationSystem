b5f1ded4860bfc4e6dcb794d2fe6515c3f9cbba3
Hồ Tuấn <hovantuan0108@gmail.com>
2025-06-02 21:38:11 +0700

    Delete backend/src directory

--- backend/src/main/java/com/swp/blooddonation/exception/exceptions/ResetPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.swp.blooddonation.exception.exceptions;
-
-public class ResetPasswordException extends RuntimeException {
-    public ResetPasswordException(String message) {
-        super(message);
-    }
-}
