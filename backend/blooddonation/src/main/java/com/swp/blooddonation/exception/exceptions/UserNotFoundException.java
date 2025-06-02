3a7df2d063cae8a41212aa574a7adb4ee5787280
Hồ Tuấn <hovantuan0108@gmail.com>
2025-06-02 21:37:20 +0700

    Delete backend/blooddonation directory

--- backend/blooddonation/src/main/java/com/swp/blooddonation/exception/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.swp.blooddonation.exception.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String massage){
-        super(massage);
-    }
-}
