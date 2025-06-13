4cf3bfaa308802587e2de937797013c77528c6a0
Tram-mah <thailambaotram06@gmail.com>
2025-06-13 13:49:31 +0700

    13/6 shot1
    tram

--- blooddonationsystem_frontend/src/pages/User.tsx
+++ blooddonationsystem_frontend/src/pages/User.tsx
@@ -4,7 +4,7 @@ import Calendar from './Calendar';
 import { Link, useNavigate } from 'react-router-dom';
 import dinoyRajKImg from './images/User/DinoyRajK.png';
 import logoBlood from './images/Logo/logo_blood.png';
-import calendar from './images/User/calendar.png';
+import calendar from './images/User/Calendar.png';
 const user = {
   name: 'User',
   avatarUrl: '' // hoặc link tới avatar
