7acfdbfde6d8af44914dd88e554e3724e73f9f6e
trung kien <nguyentotrungkien@gmail.com>
2025-07-08 23:56:10 +0700

    update Notification api

--- test/blooddonation/src/main/java/com/swp/blooddonation/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swp.blooddonation.repository;
-
-import com.swp.blooddonation.entity.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
-}
--- test/blooddonation/src/main/java/com/swp/blooddonation/repository/SlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swp.blooddonation.repository;
-
-import com.swp.blooddonation.entity.Slot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SlotRepository extends JpaRepository<Slot,Long>
-{
-
-}
