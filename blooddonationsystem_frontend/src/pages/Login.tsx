3dc33814818c4c9943770a1fda8f2165d09a1057
MinhTHB <tranhuynhbaominh26012005@gmail.com>
2025-07-15 00:31:31 +0700

    Update Login.tsx

--- blooddonationsystem_frontend/src/pages/Login.tsx
+++ blooddonationsystem_frontend/src/pages/Login.tsx
@@ -21,7 +21,7 @@ function Login() {
 
   const navigate = useNavigate();
 
-  const { login } = useAuth();
+  const { login, refetchUser } = useAuth();
   const handleLogin = async (e?: React.FormEvent) => {
     if (e) e.preventDefault();
     setIsLoading(true);
@@ -46,6 +46,7 @@ function Login() {
       setShowSuccess(true); // Show toast
       localStorage.setItem("token", data.token);
       login(data);
+      await refetchUser(); // Cập nhật lại user cho context
       setTimeout(() => {
         // Điều hướng theo role
         switch (data.role) {
