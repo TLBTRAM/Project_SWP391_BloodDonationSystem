a405c56995b3fd030ea691ce979939ec9078c4b6
Tram-mah <thailambaotram06@gmail.com>
2025-07-14 09:20:00 +0700

    14/7 s1

--- blooddonation/src/main/java/com/swp/blooddonation/api/UserAPI.java
+++ blooddonation/src/main/java/com/swp/blooddonation/api/UserAPI.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/user")
 @SecurityRequirement(name = "api")
